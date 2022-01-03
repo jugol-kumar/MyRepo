e83fdce433c9d77d18acfa517260cca95895da81
Jugol Kumar <jk23717933@gmail.com>
2022-01-03 17:30:16 +0600

    update complated

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,13 +1,4 @@
 import Dashboard from "./components/pages/Dashboard";
-
-
-import Tags from './components/pages/tags/Tag'
-import Category from './components/pages/category/Category'
-import VueXCOm from './vuex/useCom'
-
-import Adminusers from './components/pages/adminusers/AdminUser'
-
-
 import Login from './components/login'
 export const routes = [
     {
@@ -15,28 +6,7 @@ export const routes = [
       component: Dashboard
     },
     {
-      path: '/tags',
-      component: Tags
-    },
-    {
-        path: '/categories',
-        component: Category,
-    },
-    {
-        path: '/adminusers',
-        component: Adminusers,
-    },
-    {
-        path: '/vuex-page',
-        component: VueXCOm,
-    },
-
-
-    {
         path: '/login',
         component: Login,
     }
-
-
-
 ]
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,6 @@
 <?php
-
-use App\Http\Controllers\admin\CategoryController;
-use App\Http\Controllers\admin\TagController;
-use App\Http\Controllers\admin\UserController;
 use Illuminate\Support\Facades\Route;
 
-
-//tag controller and all tag routes
-//Route::post('app/create_tag',[TagController::class, 'Test'] );
-Route::post('app/create_tag',[TagController::class, 'addTag'] );
-Route::get('app/allTag', [TagController::class, 'allTag']);
-Route::post('app/edit-tag', [TagController::class, 'editTag']);
-Route::post('app/delete-tag', [TagController::class, 'deleteTag']);
-
-
-//category controller and all category routes
-Route::post('/app/category-image/upload',[CategoryController::class, 'uploadImage']);
-Route::post('app/category/delete-image', [CategoryController::class, 'deleteImage']);
-Route::post('/app/add-category', [CategoryController::class, 'addCategory']);
-Route::get('/app/all-category', [CategoryController::class, 'allCategory']);
-Route::post('app/delete-category', [CategoryController::class, 'deleteCategory']);
-Route::post('app/update-cateogry', [CategoryController::class, 'updateCategory']);
-
-//users controller and all user routes
-Route::get('/app/all-user', [UserController::class, 'index']);
-Route::post('/app/create-user', [UserController::class, 'createUser']);
 
 Route::get('/', function (){
    return view('welcome');
@@ -34,9 +10,5 @@ Route::any('{slug}', function(){
    return view('welcome');
 });
 
-
-
-
-//Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
